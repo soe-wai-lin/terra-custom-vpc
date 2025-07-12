a32ca59e4d055637a870486146de32de0a67e20d
swl <09soewailynn@gmail.com>
2025-07-13 00:22:41 +0700

    add output

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,23 @@ output "pub_sub_02_id" {
   value = aws_subnet.terra_vpc_pub_02.id
   description = "ID of terra_vpc_pub_02"
 }
+
+output "priv_sub_01_id" {
+  value = aws_subnet.terra_vpc_priv_01.id
+  description = "ID of terra_vpc_priv_01"
+}
+
+output "priv_sub_02_id" {
+  value = aws_subnet.terra_vpc_priv_02.id
+  description = "ID of terra_vpc_priv_02"
+}
+
+output "data_01_id" {
+  value = aws_subnet.terra_vpc_data_01.id
+  description = "ID of terra_vpc_data_01"
+}
+
+output "data_02_id" {
+  value = aws_subnet.terra_vpc_data_02.id
+  description = "ID of terra_vpc_data_02"
+}

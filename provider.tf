233a28d59537ca0a82fecb02d7a76ec90f157824
swl <09soewailynn@gmail.com>
2025-07-12 19:40:28 +0700

    comment out s3 backend

--- provider.tf
+++ provider.tf
@@ -5,10 +5,10 @@ terraform {
       version = "6.0.0-beta3"
     }
   }
-  backend "s3" {
-    bucket = "swlbucket01"
-    key    = "vpc/terraform"
-    region = "ap-southeast-1"
+#  backend "s3" {
+#    bucket = "swlbucket01"
+#    key    = "vpc/terraform"
+#    region = "ap-southeast-1"
     # assume_role = {
     #   role_arn = "arn:aws:iam::851725184910:user/dev01"
     # }
